1bb87ccfb8aa547c2be7684299494b77f9aec29b
Shamar <shamar.scr@gmail.com>
2019-10-11 12:32:30 -0500

    Update of HW

--- src/main/java/com/wright/ap/unit1/lesson2/CharacterAndString.java
+++ src/main/java/com/wright/ap/unit1/lesson2/CharacterAndString.java
@@ -2,11 +2,11 @@ package com.wright.ap.unit1.lesson2;
 
 public class CharacterAndString {
     public static void main(String[] args) {
-    String s1 = "Love my Glock, My Glock Wake up and Kiss me\f";
-    String s2 = "Everyday my Glock go off on a pussy boy\f";
-    String s3 = "My Glock be hoe'ing, it can't go a day without blowing a nigga\f";
-    String s4 = "But still I love my Glock, and my Glock loves me.\f";
-    String s5 = "I'm a real street nigga bitch, I'm not one of these niggas rapping on wax.\f";
+    String s1 = "Head to Jamaica\f";
+    String s2 = "Head To Sweden\f";
+    String s3 = "South Africa, Hit my boy JoBurg\f";
+    String s4 = "Slide over to Amsterdam\f";
+    String s5 = "World Domination on the way =) .\f";
 
         System.out.println(s5+ s1 + s2 + s3 + s4);
 
--- src/main/java/com/wright/secondjava/FourVariables.java
+++ src/main/java/com/wright/secondjava/FourVariables.java
@@ -3,7 +3,7 @@ package com.wright.secondjava;
 public class FourVariables {
     public static void main(String[] args) {
         byte zero = 0b0;
-        String output = "H311" + zero + " w" + zero + "rld";
+        String output = String.format("H311%s w%srld", zero, zero);
         System.out.println(output);
 
     }
